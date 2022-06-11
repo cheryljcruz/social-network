e928635d1d0793da4d859bb235ea0a2f0e625352
koiclone <cheryljcruz1@gmail.com>
2022-06-11 14:30:31 -0500

    created user model and exported to models index

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    // virtuals
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+// total count of comments & replies
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// create User model using userSchema
+const User = model('User', UserSchema);
+
+//export the user model
+module.exports: User;
