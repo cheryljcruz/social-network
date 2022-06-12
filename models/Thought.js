a8ae101244c023303f41723712237962949c71c3
koiclone <cheryljcruz1@gmail.com>
2022-06-11 21:21:05 -0500

    added thought model: thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,7 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-const reactionSchema = new Schema({
+const ReactionSchema = new Schema({
   reactionId: {
     type: Schema.Types.ObjectId,
     default: () => new Types.ObjectId(),
@@ -20,7 +20,43 @@ const reactionSchema = new Schema({
     type: Date,
     default: Date.now,
     get: (createdAtVal) =>
-      moment(createdAtVal).format('MMMM DD YYYY, h:mm:ss a'),
+      moment(createdAtVal).format('MMM DD YYYY, h:mm:ss a'),
   },
   toJSON: { getters: true },
 });
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) =>
+      moment(createdAtVal).format('MMM DD YYYY, h:mm:ss a '),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionSchema],
+
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+  id: false,
+});
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// create thought model using schema
+const Thought = model('thought', ThoughtSchema);
+
+// export thought model
+module.exports = Thought;
